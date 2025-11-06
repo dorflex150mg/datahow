22fbae9ac7968293ad2957933058f8f22dd69f77
Gabriel <gabrielpfernandez@gmail.com>
2025-11-06 15:30:24 +0100

    test(test_logs_server): add test for the metric endpoint. Checks that that the estimate is within the expected range.

--- tests/test_logs_server.rs
+++ tests/test_logs_server.rs
@@ -2,6 +2,7 @@
 mod tests {
     use reqwest::Client;
     use serde_json::json;
+    use serde_json::Value;
 
     #[actix_rt::test]
     async fn test_receive_log_entries() {
@@ -11,7 +12,16 @@ mod tests {
         let payloads = [
             json!({"timestamp": "2025-01-01T12:00:00Z", "ip": "192.168.1.1"}),
             json!({"timestamp": "2025-01-01T12:00:01Z", "ip": "192.168.1.2"}),
-            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.1"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.2"}), //repeated
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.2"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.3"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.4"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.5"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.6"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.7"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.8"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.9"}),
+            json!({"timestamp": "2025-01-01T12:00:02Z", "ip": "192.168.1.10"}),
         ];
 
         for p in payloads.iter() {
@@ -23,5 +33,20 @@ mod tests {
                 .expect("Request failed");
             assert_eq!(resp.status(), 202);
         }
+
+        let resp = client
+            .get("http://127.0.0.1:9102/metrics")
+            .send()
+            .await
+            .expect("Request failed");
+
+        assert!(resp.status().is_success());
+
+        let json: Value = resp.json().await.expect("Invalid JSON");
+
+        // Ensure the JSON has the expected key
+        assert!(json.get("unique_ip_estimate").is_some());
+        assert!(json["unique_ip_estimate"].as_f64().unwrap() > 9.0);
+        assert!(json["unique_ip_estimate"].as_f64().unwrap() < 11.0);
     }
 }
